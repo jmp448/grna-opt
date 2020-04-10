6e7369d57eb8c9f10d35c98367f0eb01e8772bc8
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-04-10 11:14:15 -0400

    Keep data offline 1

--- code/rs2.py
+++ code/rs2.py
@@ -7,8 +7,21 @@ except:
     sys.path.insert(1, "../Azimuth/")
     import azimuth.model_comparison
 
-sequences = np.array(['ACAGCTGATCTCCAGATATGACCATGGGTT', 'CAGCTGATCTCCAGATATGACCATGGGTTT', 'CCAGAAGTTTGAGCCACAAACCCATGGTCA'])
-predictions = azimuth.model_comparison.predict(sequences)
+def test():
+    sequences = np.array(['ACAGCTGATCTCCAGATATGACCATGGGTT', 'CAGCTGATCTCCAGATATGACCATGGGTTT', 'CCAGAAGTTTGAGCCACAAACCCATGGTCA'])
+    predictions = azimuth.model_comparison.predict(sequences)
 
-for i, pred in enumerate(predictions):
-    print sequences[i], pred
+    for i, pred in enumerate(predictions):
+        print sequences[i], pred
+
+def load_data():
+    sequences = np.loadtxt()
+
+def predict_efficacy():
+
+def compare_preds():
+
+def main():
+
+if __name__=="__main__":
+    main()
