bb5e081d8df2b51a84e7753ad167ae2ddd3dcc31
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-04-10 11:57:01 -0400

    try avoiding 30bp windows

--- code/rs2.py
+++ code/rs2.py
@@ -7,6 +7,7 @@ except:
     sys.path.insert(1, "../Azimuth/")
     import azimuth.model_comparison
 
+
 def test():
     sequences = np.array(['ACAGCTGATCTCCAGATATGACCATGGGTT', 'CAGCTGATCTCCAGATATGACCATGGGTTT', 'CCAGAAGTTTGAGCCACAAACCCATGGTCA'])
     predictions = azimuth.model_comparison.predict(sequences)
@@ -14,14 +15,30 @@ def test():
     for i, pred in enumerate(predictions):
         print sequences[i], pred
 
+
 def load_data():
-    sequences = np.loadtxt()
+    fname = "../data/dc_ontarget_hct116.txt"
+    sequences = np.loadtxt(fname, dtype='str', skiprows=1, usecols=4)
+    scores = np.loadtxt(fname, skiprows=1, usecols=5)
+    return sequences, scores
 
-def predict_efficacy():
 
-def compare_preds():
+def predict_efficacy(s):
+    """
+    Input: np array of strings, gRNA candidates
+    Output: np array of Rule Set 2 scores
+    """
+    predictions = azimuth.model_comparison.predict(s, pam_audit=False)
+    return predictions
+
+
+def compare_preds(scores, preds):
+
 
 def main():
+    seqs, scores = load_data()
+    preds = predict_efficacy(seqs)
+
 
 if __name__=="__main__":
     main()
