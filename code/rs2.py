03772b9e18bf901cacd702349f2348298d1a4a33
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-04-10 10:41:43 -0400

    Run actual sequence prediction

--- code/rs2.py
+++ code/rs2.py
@@ -5,4 +5,10 @@ except:
     sys.path.insert(1, "../Azimuth/")
     import azimuth.model_comparison
 
-# try: sys.path.insert(1, "../DeepCRISPR/")
+sequences = np.array(['ACAGCTGATCTCCAGATATGACCATGGGTT', 'CAGCTGATCTCCAGATATGACCATGGGTTT', 'CCAGAAGTTTGAGCCACAAACCCATGGTCA'])
+amino_acid_cut_positions = np.array([2, 2, 4])
+percent_peptides = np.array([0.18, 0.18, 0.35])
+predictions = azimuth.model_comparison.predict(sequences, amino_acid_cut_positions, percent_peptides)
+
+for i, prediction in enumerate(predictions):
+    print sequences[i], predictionß
