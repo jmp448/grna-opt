ec753499240ab8bc05f6479155f757677ffbd88f
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-04-10 10:44:07 -0400

    Try again on typo

--- code/rs2.py
+++ code/rs2.py
@@ -10,5 +10,5 @@ amino_acid_cut_positions = np.array([2, 2, 4])
 percent_peptides = np.array([0.18, 0.18, 0.35])
 predictions = azimuth.model_comparison.predict(sequences, amino_acid_cut_positions, percent_peptides)
 
-for i, prediction in enumerate(predictions):
-    print sequences[i], prediction
+for i, pred in enumerate(predictions):
+    print sequences[i], pred
