dacf263716acbfd44227a35a72678f3e0e1c86b4
jmp448 <joshpopp97@gmail.com>
2020-04-28 22:41:18 -0400

    Train azimuth take 1

--- code/visualize.R
+++ code/visualize.R
@@ -1,3 +1,4 @@
-library(ggplot2)
+library(corrplot)
 
-corrs <- data.frame("Ground Truth" = c(NA, 0.312, 0.907), "Azimuth" = c(0.312), row.names = c("Ground Truth", "Azimuth", "DeepCRISPR"), stringsAsFactors = FALSE)
+corrs <- data.frame("Experimental" = c(NA, 0.312, 0.907, 0.345), "Azimuth" = c(0.324, NA, 0.312, 0.523), "DeepCRISPR" = c(0.905, 0.330, NA, 0.350), "DeepHF"=c(0.375, 0.550, 0.383, NA), row.names = c("Experimental", "Azimuth", "DeepCRISPR", "DeepHF"))
+corrplot(as.matrix(corrs), method="color", addCoef.col = "black", na.label='-', tl.col="black")
