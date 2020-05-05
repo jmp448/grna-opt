1c6bba814bee861b7e35e406991f8880e4ce7d4a
jmp448 <joshpopp97@gmail.com>
2020-05-05 15:26:54 -0400

    Updated visualization

--- code/visualize.R
+++ code/visualize.R
@@ -1,4 +1,4 @@
 library(corrplot)
 
-corrs <- data.frame("Experimental" = c(NA, 0.312, 0.907, 0.345), "Azimuth" = c(0.324, NA, 0.312, 0.523), "DeepCRISPR" = c(0.905, 0.330, NA, 0.350), "DeepHF"=c(0.375, 0.550, 0.383, NA), row.names = c("Experimental", "Azimuth", "DeepCRISPR", "DeepHF"))
+corrs <- data.frame("Experimental" = c(NA, 0.368, 0.2, 0.771), "Azimuth" = c(0.388, NA, 0.174, 0.443), "DeepCRISPR" = c(0.182, 0.179, NA, 0.237), "DeepHF"=c(0.829, 0.457, 0.215, NA), row.names = c("Experimental", "Azimuth", "DeepCRISPR", "DeepHF"))
 corrplot(as.matrix(corrs), method="color", addCoef.col = "black", na.label='-', tl.col="black")
